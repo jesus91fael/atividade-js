c3ba8ef548bdb0f973920ddacddc743f6a6862dd
Rafael Jesus do Prado <rafaeljprado@gmail.com>
2021-06-15 15:16:35 -0300

    Feature add exercicio 19

---
+++ algoritmos-js/exercicio-19.js
@@ -0,0 +1,9 @@
+const duracao = (tempo) => {
+    let hora = tempo / 3600
+    let minutos = (tempo%3600)/60
+    let segundos = (tempo%3600)%60
+    console.log(`O evento possui ${hora.toFixed(0)}h${minutos.toFixed(0)}m${segundos.toFixed(0)}`)
+}
+
+
+duracao(7200)
