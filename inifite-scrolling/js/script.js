9e54ec93bc3a0e6e5dff651260abe1ee669e5b3e
Rafael Jesus do Prado <rafaeljprado@gmail.com>
2021-06-17 14:02:21 -0300

    Feature: Infinity

---
+++ inifite-scrolling/js/script.js
@@ -0,0 +1,59 @@
+const container = document.getElementById('c-box__container');
+
+const titles = [
+	'Lorem ipsum dolor sit amet.',
+	'Officia sit hic nihil quisquam?',
+	'In laudantium quidem hic ipsam?',
+	'Dolorum nostrum culpa magni reprehenderit?',
+	'Rerum corporis nisi ea eum.'
+]
+
+const texts = [
+	'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ex aut numquam quaerat deserunt rerum minima dolor autem reiciendis mollitia!',
+	'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere fugit repellendus soluta nobis, dolorum officiis debitis vero. Earum dignissimos maxime ipsum ratione nisi, accusamus enim!',
+	'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat nam commodi laudantium sapiente provident? Non officiis temporibus quo ipsa, voluptas eveniet accusamus excepturi, enim ratione, adipisci atque dolorem iure ad.',
+	'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, molestiae fugit laudantium porro dicta mollitia cum corrupti placeat dignissimos. Autem maiores consectetur reiciendis repudiandae architecto officia ut placeat sapiente. Perferendis, doloremque veniam dignissimos nemo recusandae ipsa sint magni, aliquid repudiandae libero quidem et facilis ad odit, quia eligendi error cum!'
+]
+
+
+window.addEventListener('scroll', () => {
+	
+	const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+	console.log('scroll', scrollTop + clientHeight, scrollHeight - 50);
+	
+	if(scrollTop + clientHeight >= scrollHeight - 5) {
+		createPost()
+	}
+});
+
+createPost();
+createPost();
+createPost();
+
+function createPost() {
+	const post = document.createElement('section');
+	post.classList.add('c-box__container');
+	post.innerHTML = `
+        <div class="c-box__desc">
+            <img src="https://placedog.net/150/200?random" alt="" />
+        </div>	
+        <div class="c-box__desc">
+                <h2 class="post-title">${randomFrom(titles)}</h2>
+                <small class="post-date">${randomDate()}</small>
+                <p class="post-excerpt">${randomFrom(texts)}</p>
+            </div>
+        </section>
+	`;
+	
+	container.appendChild(post);
+}
+
+function randomFrom(arr) {
+	return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function randomDate() {
+	const day = Math.floor(Math.random() * 27) + 1;
+	const month = Math.floor(Math.random() * 11) + 1;
+	return `${day}/${month}/2020`;
+}
