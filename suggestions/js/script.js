ae8cb206b4fb33de2b5f09efc58fc47fccdc7198
Rafael Jesus do Prado <rafaeljprado@gmail.com>
2021-06-18 17:41:27 -0300

    Chore: project suggestions

--- suggestions/js/script.js
+++ suggestions/js/script.js
@@ -1,50 +1,79 @@
-const textarea = document.querySelector('.c-txt__sug')
-const container = document.querySelector('.c-suggestions').value
+let textArea = document.querySelector('.c-txt__sug')
+let box = document.querySelector('.c-suggestions')
+let sugitem = document.querySelector('.c-sug-__items')
+let text = document.querySelector('.c-votes c-desc')
+let btnvote = document.querySelectorAll('.btn-vote')
 
-textarea.addEventListener('keypress', (e) => {
-	if(e.key === 'Enter') {
-		addSuggetions(e.target.value.trim())		
-	}
-})
+let newSug 
+let sug = []
 
-let newSuggestion
-let allSuggestions = []
-
-function addSuggetions(suggestionText) {
+const sugestions = () => {
+    let count = 1
     textArea.addEventListener('keydown', (e) => {
-        if(e.key === 'Enter'){
-            let txtAreaValue = textarea.value
+        if (e.key === 'Enter') {
+            let txtareavalor = textArea.value
             e.preventDefault()
-            newSuggestion `
+            //if (txtareavalor != '') {
+                newSug = `
                 <article class="c-sug-__items">
                     <div class="c-votes">
-                        <span class="soma"></span>
-                        <span class="c-qtd"></span>
+                        <span class="c-soma"><input type="button" class="btn-vote"></span>
+                        <span class="c-qtd">0</span>
                         <span>votes</span>
                     </div>                      
-                    <div class="c-votes c-desc">${suggestionText}</div>                                  
-                </article>  
-            `
-
-            allSuggestions = [...allSuggestions, newSuggestion]
+                    <div class="c-votes c-desc">${txtareavalor}</div>                      
+                    
+                </article> 
+                `
+            //}else{}
+            sug = [...sug, newSug]
 
             count++
-            container.innerText = count
-            container.innerHTML += newSuggestion
-            document.querySelector("textarea").value = ""
-            //suggestionsBlock.length++
+            sugitem.innerText = count
+            box.innerHTML += newSug
+            document.querySelector(".c-txt__sug").value = ""
+            
         }
     })
-
-	container.appendChild(newSuggestion);
-	
-	// increase suggestions_count
-}
-
-const init = () => {
-    addSuggetions()
     
 }
 
+let qtdvotes = document.querySelector('.c-sug__tot')
+qtdvotes.innerText = `${sug.length} Sugestions`
+
+
+
+const setVote = () => {
+    box.addEventListener('click', (e) => {
+        const btn = e.path.find(el => el.classList && el.classList.contains('btn-vote'))
+        console.log("listener container")
+        if (btn) {
+            const block = e.path.find(el => el.classList && el.classList.contains('c-sug-__items'))
+            console.log("event btn")
+            if (btn.classList.contains('voted')) {
+                btn.classList.remove('voted')
+                unvote(block)
+            } else {
+                btn.classList.add('voted')
+                vote(block)
+            }
+        }
+    })
+}
+
+const vote = (el) => {
+    let vBtn = el.querySelector('.c-qtd')
+    vBtn.innerText = +vBtn.innerText + 1
+}
+
+const unvote = (el) => {
+    let vBtn = el.querySelector('.c-qtd')
+    vBtn.innerText = +vBtn.innerText - 1
+}
+
+const init = () => {
+    sugestions()
+    setVote()
+}
 
 init()
